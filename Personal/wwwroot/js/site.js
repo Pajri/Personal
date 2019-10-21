92c188da7a5a0a31760da62cf22a323885b2befd
pajri <aprilio.pajri@gmail.com>
2019-10-21 21:44:19 +0700

    - Add text remaining

--- Personal/wwwroot/js/site.js
+++ Personal/wwwroot/js/site.js
@@ -3,50 +3,13 @@ var page = 2;
 var pageSize = 10;
 var isGettingMorePosts = false;
 
-if ($('#postlisting').length != 0) {
-    $(window).scroll(function () {
-        if (($(window).scrollTop() == $(document).height() - $(window).height()) && !isGettingMorePosts) {
-            isGettingMorePosts = true;
-            $.ajax({
-                url: '/posts/moreposts?page=' + page + '&pageSize=' + pageSize,
-                type: 'GET',
-                beforeSend: function () {
-                    $('#loadingtext').addClass('text-info');
-                    $('#loadingtext').text('Loading ...');
-                    $('#loadingtext').show();
-                },
-                success: function (data) {
-                    if (data != null && data.trim() != "") {
-                        $("#postList").append(data);
-                        page++;
-                    }
-                    $('#loadingtext').hide();
-                },
-                error: function (data) {
-                    $('#loadingtext').text('Unable to load posts');
-                    $('#loadingtext').addClass('text-danger');
-                    $('#loadingtext').fadeIn().delay(2000).fadeOut();
-                },
-                complete: function () {
-                    isGettingMorePosts = false;
-                }
-            });
-        } else {
-            $("#loadingtext").hide();
-        }
-    });
-}
-
-if ($('.post-input').length != 0) {
-    $('.post-input').focus(function () {
-        $(this).animate({ rows: 7 }, 200, 'swing');
-    });
-    $('.post-input').focusout(function () {
-        $(this).animate({ rows: 1 }, 200, 'swing');
-    });
-}
-
 $(document).ready(function () {
+    initJqueryFiler();
+    initInfiniteScroll();
+    initContentCounter();
+});
+
+function initJqueryFiler() {
     if ($('#inputImages').length != 0) {
         $('#inputImages').filer({
             limit: 3,
@@ -59,5 +22,52 @@ $(document).ready(function () {
             onSelect: null
         });
     }
-    
-});
+}
+function initInfiniteScroll() {
+    if ($('#postlisting').length != 0) {
+        $(window).scroll(function () {
+            if (($(window).scrollTop() == $(document).height() - $(window).height()) && !isGettingMorePosts) {
+                isGettingMorePosts = true;
+                $.ajax({
+                    url: '/posts/moreposts?page=' + page + '&pageSize=' + pageSize,
+                    type: 'GET',
+                    beforeSend: function () {
+                        $('#loadingtext').addClass('text-info');
+                        $('#loadingtext').text('Loading ...');
+                        $('#loadingtext').show();
+                    },
+                    success: function (data) {
+                        if (data != null && data.trim() != "") {
+                            $("#postList").append(data);
+                            page++;
+                        }
+                        $('#loadingtext').hide();
+                    },
+                    error: function (data) {
+                        $('#loadingtext').text('Unable to load posts');
+                        $('#loadingtext').addClass('text-danger');
+                        $('#loadingtext').fadeIn().delay(2000).fadeOut();
+                    },
+                    complete: function () {
+                        isGettingMorePosts = false;
+                    }
+                });
+            } else {
+                $("#loadingtext").hide();
+            }
+        });
+    }
+}
+function initContentCounter() {
+    if ($('#txtContent').length != 0) {
+        $('#textRemaining').text(1000);
+        $('#txtContent').on('keyup change', function () {
+            var max = 1000;
+            var len = $(this).val().length;
+            $('#textRemaining').text(max - len);
+        });
+
+    }
+}
+
+
